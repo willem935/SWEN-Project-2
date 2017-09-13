34e69fb643537bfe3437e4d832790ff35946042d
LeeMintz <mintz022@umn.edu>
2017-09-13 16:50:44 +1000

    stuff is gettin weird

--- Part B/swen30006/automail/Simulation.java
+++ Part B/swen30006/automail/Simulation.java
@@ -64,13 +64,15 @@ public class Simulation {
         while(MAIL_DELIVERED.size() != generator.MAIL_TO_CREATE) {
         	//System.out.println("-- Step: "+Clock.Time());
             priority = generator.step();
-            if (priority > 0) automail.robot.behaviour.priorityArrival(priority);
-            
-            try {
+            if (priority > 0) {
+            	automail.robot.behaviour.priorityArrival(priority);
             	//LEE : added print statement here, removed from robot behaviors.
             	// seems that it makes more sense to be here instead of having
             	// each robot do it individually.
             	System.out.println("T: "+Clock.Time()+" | Priority arrived");
+            }
+            
+            try {
 				automail.robot.step();
 			} catch (ExcessiveDeliveryException e) {
 				e.printStackTrace();
